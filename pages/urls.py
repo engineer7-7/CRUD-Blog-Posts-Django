105a40ea4cad44a67aadce7172efbbc6190db817
nikolaos <chatziminasnikolaos@gmail.com>
2025-07-06 14:42:41 +0300

    Remove unwanted files and folders

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import display_page, display_projects, contact, index
-
-urlpatterns = [
-    path('about/', display_page, name='about_page'),
-    path('projects/', display_projects, name='display_projects'),
-    path('contact/', contact, name='contact'),
-    path('', index, name='index')
-]
